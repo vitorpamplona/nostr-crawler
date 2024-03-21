f3aa2cc55b91e975c02fa1fd89c5f56bfa58d249
Vitor Pamplona <vitor@vitorpamplona.com>
2024-03-21 17:06:31 -0400

    Removes relays that are misbehaving

--- js/relays.js
+++ js/relays.js
@@ -9,8 +9,6 @@ const fixedRelays = [
   'wss://relay.damus.io',
   'wss://nos.lol',
   'wss://a.nos.lol',
-  'wss://nostr-relay.freeberty.net',
-  'wss://nostr-relay.wlvs.space',
   'wss://nostr.bitcoiner.social',
   'wss://nostr.blocs.fr',
   'wss://nostr.decentony.com', // paid relay	7000	npub1pp9csm9564ewzer3f63284mrd9u2zssmreq42x4rtt390zmkrj2st4fzpm
@@ -20,7 +18,6 @@ const fixedRelays = [
   'wss://nostr.wine', // paid relay	8888	npub18kzz4lkdtc5n729kvfunxuz287uvu9f64ywhjz43ra482t2y5sks0mx5sz
   'wss://filter.nostr.wine',
   'wss://inbox.nostr.wine',
-  'wss://nostr.zebedee.cloud',
   'wss://private.red.gb.net', // paid relay	8888	npub1nctdevxxuvth3sx6r0gutv4tmvhwy9syvpkr3gfd5atz67fl97kqyjkuxk
   'wss://puravida.nostr.land', // paid relay	10000	npub16k7j4mwsqm8hakjl8x5ycrqmhx89lxkfwz2xxxcw75eav7sd8ztqy2rwdn
   'wss://relay.current.fyi',
@@ -29,17 +26,21 @@ const fixedRelays = [
   'wss://relay.nostr.com.au', // paid relay	6969	npub1qqqqqrre3jxkuyj3s4m59usdyvm0umgm0lpy6cqjtwpt649sdews5q3hw7
   'wss://relay.nostr.info',
   'wss://relay.nostrati.com', // paid relay	2000	npub1qqqqqqqut3z3jeuxu70c85slaqq4f87unr3vymukmnhsdzjahntsfmctgs
-  'wss://relay.nostrich.land', // paid relay	2100	npub1vj0wlergmkcs0sz7hfks2ywj555c2s87f40squ4sqcmqpr7897fqn6mfew
   'wss://relay.nostriches.org', // paid relay 421	npub1vnmhd287pvxxk5w9mcycf23av24nscwk0da7rrfaa5wq4l8hsehs90ftlv
   'wss://relay.snort.social',
-  'wss://no.str.cr',
   'wss://nostr.oxtr.dev',
   'wss://nostr.mom',
   'wss://relay.nostr.band'
 ]
 
+const buggyRelays = new Set([
+  'wss://fonstr-test.onrender.com',
+  'wss://fiatjaf.com',
+  'wss://pyramid.fiatjaf.com'
+])
+
 var allAvailableRelays = []
 
 fetch("https://api.nostr.watch/v1/online")
      .then(response => response.json())
-     .then(json => allAvailableRelays = [... new Set(fixedRelays.concat(json))] ); 
+     .then(json => allAvailableRelays = [... new Set(fixedRelays.concat(json))].filter((url) => !buggyRelays.has(url)  ) ); 
